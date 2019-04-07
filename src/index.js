5c08b49e2bbfdfed0abef178da5096a96c081026
Ricardo <ricardofrr@gmail.com>
2019-04-07 12:17:46 -0300

    Alterado BrowserRouter para HashRouter para hospedar em um servidor não nodejs

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import { AnimatedSwitch } from 'react-router-transition';
 
 import Historico from './components/Historico';
@@ -12,7 +12,7 @@ import Main from './components/Main';
 class App extends React.Component {
     render(){
         return(
-            <BrowserRouter>
+            <HashRouter>
                 <AnimatedSwitch
                     atEnter={{ opacity: 0 }}
                     atLeave={{ opacity: 0 }}
@@ -22,7 +22,7 @@ class App extends React.Component {
                     <Route path={"/historico"} component={Historico} />
                     <Route path={"/"} exact={true} component={Main} />
                 </AnimatedSwitch>
-            </BrowserRouter>
+            </HashRouter>
         )
     }
 }
